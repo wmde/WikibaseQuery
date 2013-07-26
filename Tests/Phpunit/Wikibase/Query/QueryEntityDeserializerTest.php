38570864619f69b093900deb0ef66f57c7014561
jeroendedauw <jeroendedauw@gmail.com>
2013-07-26 15:51:38 +0200

    Implementation work on QueryEntityDeserializer
    PP'd with addshore
    
    Change-Id: Ib70beb22878d6e149370ad19bf2f4d3408fc0ae2

--- Tests/Phpunit/Wikibase/Query/QueryEntityDeserializerTest.php
+++ Tests/Phpunit/Wikibase/Query/QueryEntityDeserializerTest.php
@@ -2,6 +2,10 @@
 
 namespace Tests\Phpunit\Wikibase\Query;
 
+use Ask\Language\Description\AnyValue;
+use Ask\Language\Option\QueryOptions;
+use Ask\Language\Query;
+use Wikibase\Query\QueryEntity;
 use Wikibase\Query\QueryEntityDeserializer;
 
 /**
@@ -18,8 +22,108 @@ use Wikibase\Query\QueryEntityDeserializer;
 class QueryEntityDeserializerTest extends \PHPUnit_Framework_TestCase {
 
 	public function testCanConstruct() {
-		new QueryEntityDeserializer();
+		$this->newSimpleQueryEntityDeserializer();
 		$this->assertFalse( false );
+	}
+
+	protected function newSimpleQueryEntityDeserializer() {
+		return new QueryEntityDeserializer( $this->getMock( 'Deserializers\Deserializer' ) );
+	}
+
+	protected function newSimpleEntity() {
+		return new QueryEntity( $this->newQuery() );
+	}
+
+	protected function newQuery() {
+		return new Query(
+			new AnyValue(),
+			array(),
+			new QueryOptions( 1, 0 )
+		);
+	}
+
+	/**
+	 * @dataProvider notAQueryEntityProvider
+	 */
+	public function testAttemptToDeserializeANonQueryEntityCausesException( $notAQueryEntitySerialization ) {
+		$deserializer = $this->newSimpleQueryEntityDeserializer();
+
+		$this->setExpectedException( 'Deserializers\Exceptions\DeserializationException' );
+		$deserializer->deserialize( $notAQueryEntitySerialization );
+	}
+
+	public function notAQueryEntityProvider() {
+		$argLists = array();
+
+		$argLists[] = array( null );
+		$argLists[] = array( true );
+		$argLists[] = array( 42 );
+		$argLists[] = array( 'foo bar baz' );
+		$argLists[] = array( array( 1, 2, '3' ) );
+		$argLists[] = array( (object)array( 1, 2, '3' ) );
+
+		return $argLists;
+	}
+
+	/**
+	 * @dataProvider notAQueryEntityProvider
+	 */
+	public function testCanNotDeserializeInvalidSerialization( $notAQueryEntitySerialization ){
+		$deserializer = $this->newSimpleQueryEntityDeserializer();
+
+		$canDeserialize = $deserializer->canDeserialize( $notAQueryEntitySerialization );
+
+		$this->assertFalse( $canDeserialize );
+
+	}
+
+	public function testCallsQueryDeserializer(){
+		$queryDeserializer = $this->getMock( 'Deserializers\Deserializer' );
+
+		$queryEntitySerialzation = $this->newQueryEntitySerialization();
+		$mockQuery = $this->newQuery();
+
+		$queryDeserializer->expects( $this->once() )
+			->method( 'deserialize' )
+			->with( $this->equalTo( $queryEntitySerialzation['query'] ) )
+			->will( $this->returnValue( $mockQuery ) );
+
+		$queryDeserializer->expects( $this->once() )
+			->method( 'canDeserialize' )
+			->with( $this->equalTo( $queryEntitySerialzation['query'] ) )
+			->will( $this->returnValue( true ) );
+
+		$deserializer = new QueryEntityDeserializer( $queryDeserializer );
+
+		$deserialization = $deserializer->deserialize( $queryEntitySerialzation );
+
+		$this->assertInstanceOf( 'Wikibase\Query\QueryEntity', $deserialization, 'Returned value not instance of QueryEntity' );
+	}
+
+	public function newQueryEntitySerialization(){
+		return array(
+			'entity' => array( 'query', 1337 ),
+
+			'query' => array(
+				'objectType' => 'query',
+				'description' => array(
+					'objectType' => 'description',
+					'descriptionType' => 'anyValue',
+					'value' => array(
+						'property' => 'p1',
+					)
+				),
+				'selectionRequests' => array(),
+				'options' => array(
+					'objectType' => 'queryOptions',
+					'limit' => 10,
+					'offset' => 0,
+					'sort' => array(
+						'expressions' => array()
+					)
+				),
+			),
+		);
 	}
 
 }
--- src/Wikibase/Query/QueryEntityDeserializer.php
+++ src/Wikibase/Query/QueryEntityDeserializer.php
@@ -2,6 +2,9 @@
 
 namespace Wikibase\Query;
 
+use Deserializers\Deserializer;
+use Deserializers\Exceptions\DeserializationException;
+
 /**
  * @since 1.0
  *
@@ -14,6 +17,35 @@ namespace Wikibase\Query;
  */
 class QueryEntityDeserializer {
 
-	// TODO
+	protected $queryDeserializer;
+
+	public function __construct( Deserializer $queryDeserializer ) {
+		$this->queryDeserializer = $queryDeserializer;
+	}
+
+	public function deserialize( $serialization ) {
+		$this->assertCanSerialize( $serialization );
+
+		$query = $this->queryDeserializer->deserialize( $serialization['query'] );
+		$queryEntity = new QueryEntity( $query );
+
+		return $queryEntity;
+	}
+
+	protected function assertCanSerialize( $serialization ) {
+		if( !$this->canDeserialize( $serialization ) ) {
+			throw new DeserializationException( 'Cannot deserialize.' );
+		}
+	}
+
+	public function canDeserialize( $serialization ) {
+		if( !is_array( $serialization ) || !array_key_exists( 'query', $serialization ) ) {
+			return false;
+		}
+
+		return $this->queryDeserializer->canDeserialize( $serialization['query'] );
+	}
+
+	// TODO: finish implementation
 
 }
