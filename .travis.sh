9c5efb77c8388f42b06c48721f62b00111c3eee7
jeroendedauw <jeroendedauw@gmail.com>
2013-07-31 20:54:05 +0200

    Fix issues with TravisCI build config
    Change-Id: Iaed894c5a27efa5dce4718575746677ec47e4b27

--- .travis.sh
+++ .travis.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+originalDirectory=pwd
+
 cd ..
 
 git clone https://gerrit.wikimedia.org/r/p/mediawiki/core.git phase3 --depth 1
@@ -11,10 +13,12 @@ cd phase3
 mysql -e 'create database its_a_mw;'
 php maintenance/install.php --dbtype $DBTYPE --dbuser root --dbname its_a_mw --dbpath $(pwd) --pass nyan TravisWiki admin
 
-cd extensions
-composer create-project wikibase/query:dev-master WikibaseQuery --keep-vcs
+composer require wikibase/query:dev-master WikibaseQuery --keep-vcs
 
-cd ..
+# Replace WikibaseQuery with the version that should be tested
+rm -rf WikibaseQuery
+mkdir WikibaseQuery
+cp -r $originalDirectory/* WikibaseQuery
 
 echo 'require_once( __DIR__ . "/extensions/Wikibase/repo/Wikibase.php" );' >> LocalSettings.php
 echo 'require_once( __DIR__ . "/extensions/Wikibase/repo/ExampleSettings.php" );' >> LocalSettings.php
