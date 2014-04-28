eb0b58146eaa9f37f03406a0f57fc55f5a5aa5a8
Adrian Lang <adrian.lang@wikimedia.de>
2014-04-28 14:04:17 +0200

    Address review

--- resources/wikibase.query.SimpleQueryForm.js
+++ resources/wikibase.query.SimpleQueryForm.js
@@ -5,67 +5,91 @@
 		this.$form = $form;
 	};
 
-	SimpleQueryForm.prototype.enhance = function( options ) {
-		var $snakview, _snakview, self = this;
+	$.extend( SimpleQueryForm.prototype, {
+		/**
+		 * @var jQuery $form
+		 */
+		$form: null,
 
-		$snakview = $( '<div/>' );
-		_snakview = $snakview.snakview( {
-			entityStore: options.entityStore,
-			value: this.inputFieldValue(),
-			autoStartEditing: false,
-			locked: { snaktype: true }
-		} ).data( 'snakview' );
+		/**
+		 * Replace the form's input fields with a snakview
+		 *
+		 * @param Object options An object holding options for the snakview.
+		 *                       Currently, only options.entityStore is used.
+		 */
+		enhance: function( options ) {
+			var $snakview,
+				snakview,
+				self = this;
 
-		$snakview.on( 'snakviewafterstopediting', function() {
-			self.$form.submit();
-		} );
+			$snakview = $( '<div/>' );
+			snakview = $snakview.snakview( {
+				entityStore: options.entityStore,
+				value: this.inputFieldValue(),
+				autoStartEditing: false,
+				locked: { snaktype: true }
+			} ).data( 'snakview' );
 
-		// Don't leave space for a toolbar
-		$snakview.addClass( 'wb-claim-mainsnak' ).css( 'margin-right', '0' );
+			$snakview.on( 'snakviewafterstopediting', function() {
+				self.$form.submit();
+			} );
 
-		this.$form.find( '.wb-claim' )
-			.append( $snakview.hide() )
-			.children().toggle();
+			// Don't leave space for a toolbar
+			$snakview.addClass( 'wb-claim-mainsnak' ).css( 'margin-right', '0' );
 
-		this.$form.on( 'submit', function() {
-			self.inputFieldValue( _snakview.value() );
-		} );
+			this.$form.find( '.wb-claim' )
+				.append( $snakview.hide() )
+				.children().toggle();
 
-		_snakview.startEditing();
-	};
+			this.$form.on( 'submit', function() {
+				self.inputFieldValue( snakview.value() );
+			} );
 
-	SimpleQueryForm.prototype.inputFieldValue = function( newValue ) {
-		var $valueJson = this.$form.find( '[name="valuejson"]' ),
-			$property = this.$form.find( '[name="property"]' );
+			snakview.startEditing();
+		},
 
-		if( typeof newValue === 'undefined' ) {
-			return this._getInputFieldValue( $valueJson, $property );
-		} else {
-			return this._setInputFieldValue( $valueJson, $property, newValue );
+		/**
+		 * Get or set the current value of the field
+		 *
+		 * The argument and the return value have the following type:
+		 * { property: String, datavalue: DataValue }
+		 * If the argument is not given, the current value is returned.
+		 *
+		 * @param [Object] newValue Optional value to set
+		 */
+		inputFieldValue: function( newValue ) {
+			var $valueJson = this.$form.find( '[name="valuejson"]' ),
+				$property = this.$form.find( '[name="property"]' );
+
+			if( typeof newValue === 'undefined' ) {
+				return this._getInputFieldValue( $valueJson, $property );
+			} else {
+				return this._setInputFieldValue( $valueJson, $property, newValue );
+			}
+		},
+
+		_getInputFieldValue: function( $valueJson, $property ) {
+			var dataValue;
+			try {
+				dataValue = $.parseJSON( $valueJson.val() );
+				dataValue = dv.newDataValue( dataValue.type, dataValue.value );
+			} catch( e ) {
+				dataValue = null;
+			}
+			return {
+				property: $property.val(),
+				datavalue: dataValue
+			};
+		},
+
+		_setInputFieldValue: function( $valueJson, $property, newValue ) {
+			var dataValue = newValue.datavalue;
+			$property.val( newValue.property );
+			$valueJson.val(  $.toJSON( {
+				value: dataValue && dataValue.toJSON(),
+				type: dataValue && dataValue.getType()
+			} ) );
 		}
-	};
+	} );
 
-	SimpleQueryForm.prototype._getInputFieldValue = function( $valueJson, $property ) {
-		var dataValue;
-		try {
-			dataValue = $.parseJSON( $valueJson.val() );
-			dataValue = dv.newDataValue( dataValue.type, dataValue.value );
-		} catch( e ) {
-			dataValue = null;
-		}
-		return {
-			property: $property.val(),
-			datavalue: dataValue
-		};
-	};
-
-	SimpleQueryForm.prototype._setInputFieldValue = function( $valueJson, $property, newValue ) {
-		var dataValue = newValue.datavalue;
-		$property.val( newValue.property );
-		$valueJson.val(  $.toJSON( {
-			value: dataValue && dataValue.toJSON(),
-			type: dataValue && dataValue.getType()
-		} ) );
-	};
-
-}( jQuery, wikibase.query, dataValues, jQuery.wikibase.snakview, wikibase.store.EntityStore ) );
+}( jQuery, wikibase.query, dataValues, jQuery.wikibase.snakview ) );
