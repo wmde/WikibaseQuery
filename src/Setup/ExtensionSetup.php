24273548e2c7ec82a4eebd747c3899c722ea992d
aude <aude.wiki@gmail.com>
2014-04-30 22:53:34 +0000

    Allow accessing rootDirectory within callback

--- src/Setup/ExtensionSetup.php
+++ src/Setup/ExtensionSetup.php
@@ -93,13 +93,14 @@ class ExtensionSetup {
 			include $this->rootDirectory . '/resources/resources.php'
 		);
 
+		$rootDir = $this->rootDirectory;
 		$this->globalVars['wgHooks']['ResourceLoaderTestModules'][] = function(
 			array &$testModules,
 			\ResourceLoader &$resourceLoader
-		) {
+		) use ( $rootDir ) {
 			$testModules['qunit'] = array_merge(
 				$testModules['qunit'],
-				include( $this->rootDirectory . '/tests/qunit/resources.php' )
+				include( $rootDir . '/tests/qunit/resources.php' )
 			);
 			return true;
 		};
